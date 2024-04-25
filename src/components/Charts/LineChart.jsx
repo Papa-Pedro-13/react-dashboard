2028b9530a7f985d42b1a7aef197b162d0283caf
Papa-Pedro-13 <aleksandrostrovsqi@gmail.com>
2024-04-25 22:58:41 +0300

    makes the project structure

---
+++ src/components/Charts/LineChart.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LiteChart = () => {
+  return <div>LiteChart</div>;
+};
+
+export default LiteChart;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import './output.css'
+
 import App from './App'
 
-import './index.css'
 
 ReactDOM.render(<App />, document.getElementById("root"))
